2b489add6ce77430ea10a2c2ba4601884da7328e
Nurali <nuralisteam2009@gmail.com>
2026-06-22 20:38:28 +0500

    learn useEffect useMemo useLayotEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { useEffect , useLayoutEffect, useState } from 'react'
 import './App.css'
+import { useMemo } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
 
+  
+
+  useEffect(() => {
+    console.log('useEffect');
+  }, []);
+
+  useEffect(() => {
+    console.log('useEffect update count', count);
+  }, [count]);
+
+  useLayoutEffect(() => {
+    console.log('useLayoutEffect')
+  }, [])
+  const message = useMemo(() => {
+      console.log('useMemo');
+      if (count > 5){
+        return 'больше 5'
+      }
+    }, [count])
+    
+  console.log('render, message ', message);
   return (
     <>
+    <h3>Count: {count} </h3>
+    <button onClick={() => setCount(count +1)}>Click</button>
+    {message && <span>{message}</span>}
     </>
   )
 }
