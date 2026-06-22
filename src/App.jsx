527e835b02766624ef602c9f9972754dbc28de8d
Nurali <nuralisteam2009@gmail.com>
2026-06-22 21:26:16 +0500

    Home work

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import './App.css'
 
-
 function App() {
-  const ref = useRef(null);
-  const [value, setValue] = useState();
+  const [toggle, setToggle] = useState(true);
 
-  useEffect(() => {
-    console.log('ref', ref.current)
-    ref.current.focus();
-  }, []);
-
-  const onChancge = (e) =>{
-    setValue(e.target.value);
-  }
-
-  const checkCorrectName = useCallback(() => {
-    console.log('value', value)
-  }, [value] )
-
-  console.log('render')
   return (
     <>
-    <input placeholder="Name" ref={ref} value={value} onChange={onChancge} />
-    <button onClick={checkCorrectName}>Check</button>
+      <button onClick={() => setToggle(!toggle)}>
+        {toggle ? "вкл" : "выкл"}
+      </button>
     </>
   );
 }
 
-export default App
+export default App;
