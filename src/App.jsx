c3d4ed347445315d161d67a9e02ae8b7ffb300fd
Nurali <nuralisteam2009@gmail.com>
2026-07-01 14:39:06 +0500

    Add WelcomeMessage component and update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,12 @@
-import { useState } from 'react';
-import './App.css'
-import Button from './Button';
-import Person from './Person';
-
-
-const person = {
-  name: "nurali", secondName: "Agajanow"
-}
-
-const students = [
-  {
-    name: "Nurali",
-    secondName: ""
-  },
-
-  {
-    name: "Murad",
-    secondName: ""
-  },
-
-  {
-    name: "Alex",
-    secondName: ""
-  },
-
-  {
-    name: "Ilyas",
-    secondName: ""
-  }
-];
-
+import WelcomeMessage from './WelcomeMessage';
 
 function App() {
-  const [count, setCount] = useState(0);
 
-
-  const onClick = () => {
-  setCount(count +1);
-  };
-
-  console.log('render count', count)
-  return(
+  return (
     <>
-    <h3>Count: {count}</h3>
-    <button onClick={onClick}>Click</button>
+    <WelcomeMessage username="Нурали"/>
     </>
-  );
-
-
-  // return (
-  //   <>
-  //   <h3>Hello work</h3>
-  //   <br/>
-  //   <Button>11111</Button>
-  //   <Button/>
-
-  //   <Person name={person.name} secondName={person.secondName}/>
-
-  //   <Person {...person}/>
-
-  //   {students.map(student => <Person {...student}/>)}
-  //   </>
-  // );
+  )
 }
 
-export default App;
+export default App
