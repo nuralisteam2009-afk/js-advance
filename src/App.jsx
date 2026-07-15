1e485c65c93b215c765d318028428ab165d930de
Nurali <nuralisteam2009@gmail.com>
2026-07-15 20:58:51 +0500

    lesson-8

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
 
-function App() {
-
+export default function RegistrationForm() {
   return (
     <>
+      {/* <div className="
+      bg-cyan-700 w-40 h-50
+      hover:bg-red-600
+        // dark:bg-blue-600 
+      lg:bg-amber-600"
+      ></div > */}
+      <div className='flex flex-col md:flex-row md:justify-around gap-4'>
+        <div className='bg-fuchsia-600 md:w-40 h-40 w-screen'></div>
+        <div className='bg-fuchsia-600 md:w-40 h-40 w-screen'></div>
+        <div className='bg-[#5f2c2c] md:w-40 h-40 w-screen'></div>
+      </div>
     </>
-  )
-}
+  );
+} 
 
-export default App
